ba7d32ed789ff93979ba5f967a7592e30148fb40
neelpatel-dev <neel.patel@aven.com>
2026-02-17 18:55:40 -0800

    Update formula sha256

--- Formula/ubo.rb
+++ Formula/ubo.rb
@@ -2,7 +2,7 @@ class Ubo < Formula
   desc "One-command uBlock Origin installer for Google Chrome on macOS"
   homepage "https://github.com/neel49/ubo"
   url "https://github.com/neel49/ubo/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "2e8c1e057ad93baa81b4e41659006c129d1faff3ed0489400d461cd02644bd71"
+  sha256 "d839a48038446c4454a59e345df8191d9a5afc2dc0025fd8c4adb567b4ec4a00"
   license "MIT"
 
   depends_on :macos
